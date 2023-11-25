901057cbe3d6b9aa40d1a7cb7f7980174f0dee95
Achmad Raihan Fahrezi Effendy <raihanachmad8@gmail.com>
2023-11-25 20:37:42 +0700

    (chore): Set up project structure with React and Express
    Added configuration files (.editorconfig, .env.example, .gitignore),
    created directories for frontend and backend,
    and initialized package.json for managing project dependencies.

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = new express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Hello from the server!' });
+});
+
+app.listen(port, host, () => {
+    console.log(`Example app listening at http://${host}:${port}`)
+    }
+)
